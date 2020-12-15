4d779ea409dc1d41ccec9a988b42679f8c501b52
Sreeraj Valapil <sreeraj.valapil@consultant.volvo.com>
2020-12-15 22:18:43 +0100

    Added comment , renamed RBI to HDFC

--- src/main/java/com/example/testspringboot/rest/BankController.java
+++ src/main/java/com/example/testspringboot/rest/BankController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+/*This is the Spring Controller */
+@RestController
 public class BankController {
 
   @RequestMapping("/branch")
   public String getMainBranchName() {
-    return "RBI INDIA " ;
+    return "HDFC" ;
   }
 }
